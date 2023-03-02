cd406a035b989afeefcab499dc0446c1303a2cbe
Rashko Mollov <rashko.1986@gmail.com>
2023-03-02 11:06:51 +0200

    Solved tasks

---
+++ 08. Classes and Attributes - Lab/04. Point Distance.js
@@ -0,0 +1,18 @@
+class Point {
+    constructor(x, y) {
+        this.x = x,
+        this.y = y
+    }
+
+    static distance(point1, point2){
+        const x = point2.x - point1.x;
+        const y = point2.y - point1.y;
+        const distance = Math.sqrt(x ** 2 + y ** 2);
+        
+        return distance;
+    }
+}
+
+let p1 = new Point(5, 5);
+let p2 = new Point(9, 8);
+console.log(Point.distance(p1, p2));
