d6b41db5cdc87d45ed5824c2950bb2ea9dd6503a
Rashko Mollov <rashko.1986@gmail.com>
2023-03-14 15:19:55 +0200

    Refactored Forecast task

--- JS Applications 2023/02. Asynchronous Programming - Exercise/03.Forecaster/app.js
+++ JS Applications 2023/02. Asynchronous Programming - Exercise/03.Forecaster/app.js
@@ -21,6 +21,10 @@ function attachEvents() {
         try {
             label[0].textContent = 'Current conditions';
             label[1].textContent = 'Three-day forecast';
+            // Resets div for new data 
+            document.querySelectorAll('div[class="forecasts"]').forEach(x => x.remove());
+            document.querySelectorAll('div[class="forecast-info"]').forEach(x => x.remove());
+
             const locationUrl = `http://localhost:3030/jsonstore/forecaster/locations`;
             const response = await fetch(locationUrl);
             const data = await response.json();
@@ -33,44 +37,22 @@ function attachEvents() {
 
             forecastDiv.style.display = 'block';
 
-            const div = document.createElement('div');
-            div.setAttribute('class', 'forecasts');
-
-            const symbolSpan = document.createElement('span');
-            symbolSpan.setAttribute('class', 'condition symbol')
-
             const currCondition = weatherData.forecast.condition;
-            let weatherIcon = '';
+            const div = create('div', { className: 'forecasts' });
+            const symbolSpan = create('span', { className: 'condition symbol' }, currCondition);
+            const spanGroup = create('span', { className: 'condition' });
+            const citySpan = create('span', { className: 'forecast-data' });
+            citySpan.textContent = weatherData.name;
 
-            if (currCondition == 'Sunny') {
-                weatherIcon = weatherIcons['sunny'];
-            } else if (currCondition == 'Partly sunny') {
-                weatherIcon = weatherIcons['partlySunny'];
-            } else if (currCondition == 'Overcast') {
-                weatherIcon = weatherIcons['overcast'];
-            } else if (currCondition == 'Rain') {
-                weatherIcon = weatherIcons['rain'];
-            }
-            symbolSpan.innerHTML = weatherIcon;
-
-            const spanGroup = document.createElement('span');
-            spanGroup.setAttribute('class', 'condition');
-
-            const citySpan = document.createElement('span');
-            citySpan.setAttribute('class', 'forecast-data');
-            citySpan.textContent = weatherData.name
-
-            const degreesSpan = document.createElement('span');
-            degreesSpan.setAttribute('class', 'forecast-data');
+            const degreesSpan = create('span', { className: 'forecast-data' });
             degreesSpan.innerHTML = `${weatherData.forecast.low}${weatherIcons['degrees']}/${weatherData.forecast.high}${weatherIcons['degrees']}`;
 
-            const iconSpan = document.createElement('span');
-            iconSpan.setAttribute('class', 'forecast-data');
-            iconSpan.textContent = currCondition;
+            const weatherTextSpan = create('span', { className: 'forecast-data' });
+            weatherTextSpan.textContent = currCondition;
 
             spanGroup.appendChild(citySpan);
             spanGroup.appendChild(degreesSpan);
-            spanGroup.appendChild(iconSpan);
+            spanGroup.appendChild(weatherTextSpan);
             div.appendChild(symbolSpan);
             div.appendChild(spanGroup);
             currentDiv.appendChild(div);
@@ -80,50 +62,50 @@ function attachEvents() {
             const upcomingResponse = await fetch(upcomingUrl);
             const upcomingData = await upcomingResponse.json();
 
-            const divUpcoming = document.createElement('div');
-            divUpcoming.setAttribute('class', 'forecast-info');
+            const divUpcoming = create('div', { className: 'forecast-info' });
             upcomingDiv.appendChild(divUpcoming);
+
             Object.entries(upcomingData.forecast).forEach(x => {
-
-
-                const spanGroup = document.createElement('span');
-                spanGroup.setAttribute('class', 'upcoming');
-
                 const currCondition = x[1].condition;
-                let weatherIcon = '';
-
-                if (currCondition == 'Sunny') {
-                    weatherIcon = weatherIcons['sunny'];
-                } else if (currCondition == 'Partly sunny') {
-                    weatherIcon = weatherIcons['partlySunny'];
-                } else if (currCondition == 'Overcast') {
-                    weatherIcon = weatherIcons['overcast'];
-                } else if (currCondition == 'Rain') {
-                    weatherIcon = weatherIcons['rain'];
-                }
-
-                const symbolSpan = document.createElement('span');
-                symbolSpan.setAttribute('class', 'symbol')
-                symbolSpan.innerHTML = weatherIcon;
-
-                const degreesSpan = document.createElement('span');
-                degreesSpan.setAttribute('class', 'forecast-data');
+                
+                const spanGroup = create('span', { className: 'upcoming'});              
+                const symbolSpan = create('span', { className: 'symbol' }, currCondition);
+                const degreesSpan = create('span', { className: 'forecast-data' });
                 degreesSpan.innerHTML = `${x[1].low}${weatherIcons['degrees']}/${x[1].high}${weatherIcons['degrees']}`;
 
-                const weather = document.createElement('span');
-                weather.setAttribute('class', 'forecast-data');
+                const weather = create('span', { className: 'forecast-data' });
                 weather.textContent = currCondition;
 
                 spanGroup.appendChild(symbolSpan);
                 spanGroup.appendChild(degreesSpan);
                 spanGroup.appendChild(weather);
-                divUpcoming.appendChild(spanGroup);        
+                divUpcoming.appendChild(spanGroup);
             });
 
         } catch (error) {
             forecastDiv.style.display = 'block';
             label.forEach(x => x.textContent = 'Error');
         }
+    }
+
+    function create(type, attr, weatherCondition) {
+        const element = document.createElement(type);
+        Object.assign(element, attr);
+
+        if (weatherCondition != undefined) {
+            let weatherIcon = '';
+            if (weatherCondition == 'Sunny') {
+                weatherIcon = weatherIcons['sunny'];
+            } else if (weatherCondition == 'Partly sunny') {
+                weatherIcon = weatherIcons['partlySunny'];
+            } else if (weatherCondition == 'Overcast') {
+                weatherIcon = weatherIcons['overcast'];
+            } else if (weatherCondition == 'Rain') {
+                weatherIcon = weatherIcons['rain'];
+            }
+            element.innerHTML = weatherIcon;
+        }
+        return element;
     }
 }
 
